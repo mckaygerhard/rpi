b9d5493dac5ec0a34f9eb61ef68995dbe48915e1
Stefan Mititelu <stefan.mititelu@onem.com>
2018-02-14 17:26:57 +0200

    Add uas_start_bg

--- sipp/uas.sh
+++ sipp/uas.sh
@@ -38,6 +38,24 @@ uas_start() {
 		$SRV_SIP_IP:$SRV_SIP_PORT
 }
 
+uas_start_bg() {
+	# UAS vars
+	DIR_TEST=$1
+
+	# UAS call
+	sipp	-aa -nd					\
+		-r $MESSAGES_RATE			\
+		-rp $MESSAGES_RATE_PERIOD		\
+		-d $MESSAGES_PAUSE			\
+		-m $MESSAGES				\
+		-i $UAS_SIP_IP -p $UAS_SIP_PORT		\
+		-mi $UAS_RTP_IP -mp $UAS_RTP_PORT	\
+		-sf "$DIR_TEST/$UAS_XML"		\
+		-inf "$DIR_CSV/$UAS_CSV"		\
+		-key "uas_pcap" "$DIR_PCAP/$UAS_PCAP"   \
+		$SRV_SIP_IP:$SRV_SIP_PORT &> /dev/null &
+}
+
 uas_check $@
 uas_start $@
 #uas_start_bg $@
